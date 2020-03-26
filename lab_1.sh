2f64f15c7e5b572b45cb79f4fda83b6d3e244502
NightFozy <62706468+NightFozy@users.noreply.github.com>
2020-03-26 17:07:15 +0300

    Add files via upload

--- lab_1.sh
+++ lab_1.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-function table {  #функция для обработки файлов в директории
+function table {  #функция для обработки файлов
  for file in "$1"/*  #цикл для перебора файлов
   do
   this_file="${file##*/}" #выделение имени файла из абсолютного пути
   if [ -d "$file" ] #проверка - является ли файл директорией
   then
-   table "$file" #рекурсивный вызов функции для обработки файлов во вложенной директории
+   table "$file" #рекурсивный вызов функции для вложенной директории
   else
-   name="${this_file%.[^.]*}" #выделение имени файла с помощью регулярного выражения
+   name="${this_file%.[^.]*}" #выделение имени файла
    extension="${this_file##*.}" #выделение расширения файла
    data_of_change=$(date +%Y-%m-%d -r "$file" 2>/dev/null)  #дата последнего изменения
    if [[ -z "$name" && -n "$extension" ]]; then          #проверка для файлов без имени
@@ -18,8 +18,8 @@ function table {  #функция для обработки файлов в ди
    size=$(wc -c 2>/dev/null <"$file" | awk '{print $1}' ) #получение размера файла в байтах
    let "size = "$size"  /1024"                            #в килобайтах
    duration_sec=$(ffmpeg -i $file 2>&1 | grep Duration | awk '{print $2}') # получение длины видео
-   filepath="${file%/*}" #получение пути к файлу
-   echo -e "$name \t $extension \t $data_of_change \t $size "kB"  \t $filepath \t $duration_sec"  >> result.xls #дозапись строки вывода в файл .xls
+   filepath="${file%/*}"                                                   #получение пути к файлу
+   echo -e "$name \t $extension \t $data_of_change \t $size "kB"  \t $filepath \t $duration_sec"  >> result.xls #дозапись строки вывода в файл
    fi
   done
 }
